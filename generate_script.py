a3b189500e5b8b55e6994a5da0158948d13fdef6
ShunsukeTamaki <shunsuke.tamaki@fiertecorp.com>
2023-08-11 15:17:52 +0900

    update/generate_script.py 複数ファイルに対応

--- generate_script.py
+++ generate_script.py
@@ -1,23 +1,37 @@
 import whisper
 import datetime
 import sys 
+import os
+sys.stdout.reconfigure(encoding='utf-8')
 
 
-# ファイルの取り込み
-path = sys.argv[1]
-# モデルのモード選び(精度が上がる)
-model_names = ["tiny", "base", "small", "medium", "large"]
-model = whisper.load_model(model_names[0])
-# 読み込み(verbose=Trueにすると時刻のログが出る)
-result = model.transcribe(path, verbose=False, language='ja')
+def main():
+    # ファイルの取り込み
+    files = sys.argv[1:]
+    # モデルのモード選び(精度が上がる)
+    model_names = ["tiny", "base", "small", "medium", "large"]
+    model_level = int(input("モデルのサイズを1~5で選択してください：")) - 1
+    print("モデル読み込み中")
+    model = whisper.load_model(model_names[model_level])
+    print("モデル読み込み完了")
 
-# 会話ログをtxtファイルに書き出し(タイムスタンプ有)
-t_delta = datetime.timedelta(hours=9)
-JST = datetime.timezone(t_delta, 'JST')
-now = datetime.datetime.now(JST)
-f = open(f"台本/{now.strftime('%Y%m%d%H%M%S')}.txt", 'x')
-for seg in result["segments"]:
-  start = datetime.timedelta(seconds=int(seg["start"]))
-  end = datetime.timedelta(seconds=int(seg["end"]))
-  f.write(f'{start}-{end} {seg["text"]}\n')
-f.close()
+    # 全てのファイルに対して台本を生成
+    for i, file in enumerate(files):
+        # 読み込み(verbose=Trueにすると時刻のログが出る)
+        print(f"台本生成中")
+        result = model.transcribe(file, verbose=False, language='ja')
+        print(f"台本生成完了 {i+1}/{len(files)}")
+        # 台本ファイルの生成
+        file_name = os.path.basename(file)
+        t_delta = datetime.timedelta(hours=9)
+        JST = datetime.timezone(t_delta, 'JST')
+        now = datetime.datetime.now(JST)
+        f = open(f"台本/{file_name}_{now.strftime('%Y%m%d%H%M%S')}.txt", 'x')
+        for seg in result["segments"]:
+            start = datetime.timedelta(seconds=int(seg["start"]))
+            end = datetime.timedelta(seconds=int(seg["end"]))
+            f.write(f'{start}-{end} {seg["text"]}\n')
+        f.close()
+
+if __name__ == "__main__":
+    main()
