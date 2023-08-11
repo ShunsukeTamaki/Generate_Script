d23cbb3d97db856338b7268630505d9a98eb5ca5
ShunsukeTamaki <shunsuke.tamaki@fiertecorp.com>
2023-08-11 15:17:20 +0900

    update/run.bat 複数ファイルに対応

--- run.bat
+++ run.bat
@@ -3,7 +3,7 @@ rem この行の下に、generate_script.py のあるディレクトリに移動
 cd /d C:\Github\Generate_Script
 
 rem Python スクリプトの実行
-pipenv run python generate_script.py %1
+pipenv run python generate_script.py %*
 
 rem 画面を開いていて欲しくない場合は、以下の行をコメントアウトもしくは削除してください
 pause
