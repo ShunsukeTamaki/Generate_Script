dfb2238b40160e1873536c84209e831e42bd027b
ShunsukeTamaki <shunsuke.tamaki@fiertecorp.com>
2023-08-11 14:42:52 +0900

    add/run.bat

---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+rem この行の下に、generate_script.py のあるディレクトリに移動するコマンドを追加
+cd /d C:\Github\Generate_Script
+
+rem Python スクリプトの実行
+pipenv run python generate_script.py %1
+
+rem 画面を開いていて欲しくない場合は、以下の行をコメントアウトもしくは削除してください
+pause
